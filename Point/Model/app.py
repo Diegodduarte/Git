cc9b252d5fa89faa7c171a89f4d30f71c271e183
Diegodduarte <32546703+Diegodduarte@users.noreply.github.com>
2017-10-21 00:28:51 -0300

    Modificaçoes no escopo

--- Point/Model/app.py
+++ Point/Model/app.py
@@ -1,7 +1,15 @@
 from point import *
 
-P = Point
+P = Point()
 
 
-Point.SolicitarPontos()
+P1 = Point()
+P2 = Point()
 
+P1.SolicitarPontos()
+P2.SolicitarPontos()
+PN = Point()
+
+PN = P1 + P2
+
+print PN
